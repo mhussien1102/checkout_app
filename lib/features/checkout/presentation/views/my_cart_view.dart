d449683581239abe9b471698b3805107edad719d
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-23 17:52:33 +0300

    create the app bar

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -5,6 +5,8 @@ class MyCartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Cart'), centerTitle: true),
+    );
   }
 }
