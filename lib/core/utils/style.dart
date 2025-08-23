0acf2038c3d744d2890a6541e132e167a5abf5b0
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-23 18:57:15 +0300

    add the fonts and style the appbar

--- lib/core/utils/style.dart
+++ lib/core/utils/style.dart
@@ -1,59 +1,56 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-abstract class Style {
-  static TextStyle textStyle25w500 = TextStyle(
+abstract class AppTextStyles {
+  static const TextStyle text25w500 = TextStyle(
     color: Colors.black,
     fontSize: 25,
     fontWeight: FontWeight.w500,
-    height: 0,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle18w400 = TextStyle(
+  static const TextStyle text18w400 = TextStyle(
     color: Colors.black,
     fontSize: 18,
     fontWeight: FontWeight.w400,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle24w600 = TextStyle(
+  static const TextStyle text24w600 = TextStyle(
     color: Colors.black,
     fontSize: 24,
     fontWeight: FontWeight.w600,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle22w500 = TextStyle(
+  static const TextStyle text22w500 = TextStyle(
     color: Colors.black,
     fontSize: 22,
     fontWeight: FontWeight.w500,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle20w600 = TextStyle(
+  static const TextStyle text20w600 = TextStyle(
     color: Colors.black,
     fontSize: 20,
     fontWeight: FontWeight.w600,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle12w600 = TextStyle(
+  static const TextStyle text12w600 = TextStyle(
     color: Colors.black,
     fontSize: 12,
     fontWeight: FontWeight.w600,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle18w600 = TextStyle(
+  static const TextStyle text18w600 = TextStyle(
     color: Colors.black,
     fontSize: 18,
     fontWeight: FontWeight.w600,
     fontFamily: "Inter",
   );
 
-  static TextStyle textStyle20w400 = TextStyle(
+  static const TextStyle text20w400 = TextStyle(
     color: Colors.black,
     fontSize: 20,
     fontWeight: FontWeight.w400,
--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/style.dart';
+
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Cart'), centerTitle: true),
+      appBar: AppBar(
+        title: Text('My Cart', style: AppTextStyles.text25w500),
+        centerTitle: true,
+      ),
     );
   }
 }
