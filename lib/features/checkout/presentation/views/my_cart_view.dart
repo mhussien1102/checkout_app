bdb695f0a738aa555a15a8bab28de74d3a235a11
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-23 20:31:28 +0300

    create Appbar of first screen

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../../core/utils/style.dart';
 
@@ -9,6 +10,14 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Center(
+          child: GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset('assets/icons/Arrow 1.svg'),
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
         title: Text('My Cart', style: AppTextStyles.text25w500),
         centerTitle: true,
       ),
