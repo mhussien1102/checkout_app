422817e79fde703b63a1f50ae061fcf7d411378a
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-26 11:58:25 +0300

    make the Custom Shaped Container

--- lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -5,6 +5,30 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              color: Color(0xffD9D9D9),
+            ),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            left: -20,
+            child: CircleAvatar(backgroundColor: Colors.white),
+          ),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            right: -20,
+            child: CircleAvatar(backgroundColor: Colors.white),
+          ),
+        ],
+      ),
+    );
   }
 }
